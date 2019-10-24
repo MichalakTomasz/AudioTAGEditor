a02a3570b199cff5284279974f96c5a3741ba707
Electrodeus <vlad3dracula@gmail.com>
2019-10-24 11:39:55 +0200

    Fixed ComparerService

--- AudioTAGEditor/Services/IAudioFileComparerService.cs
+++ AudioTAGEditor/Services/IAudioFileComparerService.cs
@@ -1,10 +1,13 @@
 ﻿using AudioTAGEditor.Models;
+using System.Collections.Generic;
 
 namespace AudioTAGEditor.Services
 {
     public interface IAudiofileComparerService
     {
         bool AreTheSame(Audiofile sourcefile, Audiofile fileToCompare);
+        bool AreTheSame(IEnumerable<Audiofile> sourceAudiofiles, IEnumerable<Audiofile> filesToCompare);
         bool AreTheSameTags(Audiofile sourcefile, Audiofile fileToCompare);
+        bool AreTheSameTags(IEnumerable<Audiofile> sourceAudiofiles, IEnumerable<Audiofile> filesToCompare);
     }
 }
